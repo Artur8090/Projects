0bb0ad06fe2309e342d52cd5d69d48251b37b9f4
Artur8090 <artur.cheuchiev@gmail.com>
2024-02-12 21:48:28 +0100

    Calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,18 +1,33 @@
-function calculator(x,y,opperator){
-    if(opperator == "+"){
-        return x + y;
-    } else if(opperator == "-"){
-        return x - y;
-    } else if(opperator == "*"){
-        return x * y;
-    } else if(opperator == "/"){
-        return x / y;
-    } else if(opperator == "%"){
-        return (x / y) * 100 + "%"
-    }
+let addition = "+";
+let subtract = "-"
+let multiply = "*"
+let divide = "/"
+let percentage = "%";
+let equal = "=";
+
+let opp = 0;
+let opperator;
+let eq1 = 0;
+let eq2 = 0;
+let equation = [];
+let strEquation;
+function add(x){
+    equation.push(x);
+        if(equation[0] == "+" || equation[0] == "*" || equation[0] == "/" || equation[0] == "%"){
+            return document.querySelector(".answer").innerText = "Error";
+        } 
+    strEquation = equation.join("")
+    document.querySelector(".answer").innerText = strEquation;
 }
-console.log(calculator(10,20,"+"))
-console.log(calculator(10,20,"-"))
-console.log(calculator(10,20,"*"))
-console.log(calculator(10,20,"/"))
-console.log(calculator(10,20,"%"))
+
+
+function equate(){
+    for(let i = 0; i != equation.length; i++){
+        if(typeof equation[i] === "str" && typeof equation[i + 1] === "str"){
+            return document.querySelector(".answer").innerText = "Error";
+        } else {
+            strEquation = eval(strEquation);
+            document.querySelector(".answer").innerText = strEquation;
+        }
+    } 
+}
