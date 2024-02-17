90315e9011e0dd7083d57ba66a108366b9c3a95e
Artur <artur,cheuchiev@gmail.com>
2024-02-17 14:45:05 +0100

    calculator new my code

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,30 +4,54 @@ let multiply = "*"
 let divide = "/"
 let percentage = "%";
 let equal = "=";
-
-let opp = 0;
-let opperator;
-let eq1 = 0;
-let eq2 = 0;
+let num1 = 0;
+let num2 = 0;
 let equation = [];
-let strEquation;
+let answer = 0;
 function add(x){
+
     equation.push(x);
         if(equation[0] == "+" || equation[0] == "*" || equation[0] == "/" || equation[0] == "%"){
             return document.querySelector(".answer").innerText = "Error";
         } 
-    strEquation = equation.join("")
+        strEquation = equation.join("");
     document.querySelector(".answer").innerText = strEquation;
-}
 
+    }
+ 
 
 function equate(){
-    for(let i = 0; i != equation.length; i++){
-        if(typeof equation[i] === "str" && typeof equation[i + 1] === "str"){
-            return document.querySelector(".answer").innerText = "Error";
-        } else {
-            strEquation = eval(strEquation);
-            document.querySelector(".answer").innerText = strEquation;
-        }
-    } 
+if(equation.includes("+")){
+    equation = equation.join("").split("+");
+    num1 += parseInt(equation[0]);
+    num2 += parseInt(equation[1]);
+    answer = num1 + num2;
+} else if(equation.includes("-")){
+    equation = equation.join("").split("-");
+    num1 += parseInt(equation[0]);
+    num2 += parseInt(equation[1]);
+    answer = num1 - num2;
+} else if(equation.includes("*")){
+    equation = equation.join("").split("*");
+    num1 += parseInt(equation[0]);
+    num2 += parseInt(equation[1]);
+    answer = num1 * num2;
+} else if(equation.includes("/")){
+    equation = equation.join("").split("/");
+    num1 += parseInt(equation[0]);
+    num2 += parseInt(equation[1]);
+    answer = num1 / num2;
+} else if(equation.includes("%")){
+    equation = equation.join("").split("%");
+    num1 += parseInt(equation[0]);
+    num2 += parseInt(equation[1]);
+    answer = (num1/100)*num2;
+}
+
+equation = [];
+ num1 = 0;
+ num2 = 0;
+ equation.push(answer)
+ console.log(answer)
+ document.querySelector(".answer").innerText = answer;
 }
