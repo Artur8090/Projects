573de37b456592ec8758b8abc440798408295bd7
Artur <artur,cheuchiev@gmail.com>
2024-02-18 13:02:36 +0100

    New Calculator, new code

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,57 +1,74 @@
-let addition = "+";
-let subtract = "-"
-let multiply = "*"
-let divide = "/"
-let percentage = "%";
-let equal = "=";
-let num1 = 0;
-let num2 = 0;
 let equation = [];
-let answer = 0;
-function add(x){
+let number = "";
 
-    equation.push(x);
-        if(equation[0] == "+" || equation[0] == "*" || equation[0] == "/" || equation[0] == "%"){
-            return document.querySelector(".answer").innerText = "Error";
-        } 
-        strEquation = equation.join("");
-    document.querySelector(".answer").innerText = strEquation;
+function pressButton(c) {
 
+    if (isOp(c)) {
+        if(number.length>0){
+            equation.push(number)
+        }
+
+        if(c != '='){
+            equation.push(c);
+        }
+        
+        console.log('eq', equation)
+        number=""
+    } else {
+        number+=c;
     }
- 
 
-function equate(){
-if(equation.includes("+")){
-    equation = equation.join("").split("+");
-    num1 += parseInt(equation[0]);
-    num2 += parseInt(equation[1]);
-    answer = num1 + num2;
-} else if(equation.includes("-")){
-    equation = equation.join("").split("-");
-    num1 += parseInt(equation[0]);
-    num2 += parseInt(equation[1]);
-    answer = num1 - num2;
-} else if(equation.includes("*")){
-    equation = equation.join("").split("*");
-    num1 += parseInt(equation[0]);
-    num2 += parseInt(equation[1]);
-    answer = num1 * num2;
-} else if(equation.includes("/")){
-    equation = equation.join("").split("/");
-    num1 += parseInt(equation[0]);
-    num2 += parseInt(equation[1]);
-    answer = num1 / num2;
-} else if(equation.includes("%")){
-    equation = equation.join("").split("%");
-    num1 += parseInt(equation[0]);
-    num2 += parseInt(equation[1]);
-    answer = (num1/100)*num2;
+    console.log('number:', number)
+    showAnswer(convertEqToString(equation, number))
 }
 
-equation = [];
- num1 = 0;
- num2 = 0;
- equation.push(answer)
- console.log(answer)
- document.querySelector(".answer").innerText = answer;
+function convertEqToString(equation, currNumber){
+    return equation.join("")+currNumber
+}
+
+function equate() {
+    pressButton("=")
+    let answer
+    console.log('cal equation', equation.length)
+
+    while(equation.length>=3){
+         answer = calc(equation[0], equation[2], equation[1])
+         equation[2] = ''+answer
+         equation = equation.splice(2,equation.length)
+         console.log('eq', equation)
+         console.log('eq', equation)
+    }
+
+    showAnswer(equation[0]);
+    equation=[]
+    equation.push(answer)
+}
+
+function calc(num1,num2,opp){
+    let number1 = parseInt(num1);
+    let number2 = parseInt(num2);
+
+    if (opp == "+") {
+        return  number1 + number2;
+    } else if (opp == "-") {
+        return  number1 - number2;
+    } else if (opp == "*") { 
+        return  number1 * number2;
+    } else if (opp == "/") {
+        return  number1 / number2;
+    } else if (opp == "%") {
+        return (number1 / 100) * number2;
+    }
+}
+
+
+function showAnswer(answer){
+    document.querySelector(".answer").innerText = answer;
+}
+
+function isOp(c){
+    if (c == "+" || c == "-" || c == "*" || c == "/" || c == "%" || c == "=") {
+        return true;
+    }
+    false
 }
